422ac0f4cb58174383190a2ba9ad45ceb4d05891
Adam Boseley <adam.boseley@gmail.com>
2015-10-23 17:07:34 +1000

    - fix "<" operator bug for map key type - switch to std::maps

--- qt5test/QTimerInterface/QTimerInterface.cpp
+++ qt5test/QTimerInterface/QTimerInterface.cpp
@@ -1,8 +1,9 @@
 #include "QTimerInterface.h"
 #include "TimedStatemachineInterface.h"
-#include <QTimerEvent>
 
+#include <QTimerEvent>
 #include <QDebug>
+#include <QtGlobal>
 
 QTimerInterface::QTimerInterface(QObject* parent) :
         QObject(parent)
@@ -18,15 +19,30 @@ QTimerInterface::~QTimerInterface()
 void QTimerInterface::setTimer(TimedStatemachineInterface* statemachine,
         sc_eventid event, sc_integer time_ms, sc_boolean isPeriodic)
 {
+    CallbackInfo callback_info;
+    callback_info.m_event = event;
+    callback_info.m_periodic = isPeriodic;
+    callback_info.m_state_machine = statemachine;
+
+    callback_reverse_map_type::iterator it = m_timers_lookup.find(callback_info);
+    if ( it != m_timers_lookup.end() )
+    {
+        qDebug() << "setTimers : orig "
+                 << " event: " << it->first.m_event;
+
+        qDebug() << "setTimers : "
+                 << " event: " << callback_info.m_event;
+
+        Q_ASSERT(  false );
+
+        unsetTimer(statemachine,event);
+    }
 
     int qt_timer = startTimer(time_ms);
-    CallbackInfo callback_info =
-    { statemachine, event, isPeriodic };
-
     m_timers[qt_timer] = callback_info;
     m_timers_lookup[callback_info] = qt_timer;
-}
 
+}
 
 void QTimerInterface::timerEvent(QTimerEvent *event)
 {
@@ -36,14 +52,7 @@ void QTimerInterface::timerEvent(QTimerEvent *event)
         if (it != m_timers.end())
         {
             // make the state machine call back
-            it->m_state_machine->raiseTimeEvent(it->m_event);
-            // stop the timer if it's not periodic
-            if (!(it->m_periodic))
-            {
-                killTimer(event->timerId());
-                m_timers_lookup.remove(*it);
-                m_timers.erase(it);
-            }
+            it->second.m_state_machine->raiseTimeEvent(it->second.m_event);
             emit timeout();
         }
     }
@@ -52,24 +61,34 @@ void QTimerInterface::timerEvent(QTimerEvent *event)
 void QTimerInterface::unsetTimer(TimedStatemachineInterface* statemachine,
         sc_eventid event)
 {
-    CallbackInfo unset_timer =
-    { statemachine, event, false };
+
+    CallbackInfo unset_timer;
+    unset_timer.m_periodic = false;
+    unset_timer.m_state_machine = statemachine;
+    unset_timer.m_event = event;
 
     callback_reverse_map_type::iterator it = m_timers_lookup.find(unset_timer);
     if (it != m_timers_lookup.end())
     {
-        killTimer(*it);
-        m_timers.remove(*it);
+        killTimer(it->second);
+        m_timers.erase(it->second);
         m_timers_lookup.erase(it);
+    }
+    else
+    {
+        qDebug() << "unsetTimersFailed : "
+                 << " event: " << unset_timer.m_event
+                 << " sm: " << unset_timer.m_state_machine ;
+        Q_ASSERT( false );
     }
 }
 
 void QTimerInterface::cancel()
 {
-    for (callback_map_type::iterator it = m_timers.begin();
-            it != m_timers.end(); it++)
+    for (callback_reverse_map_type::iterator it = m_timers_lookup.begin();
+            it != m_timers_lookup.end(); it++)
     {
-        killTimer(it.key());
+        killTimer(it->second);
     }
     m_timers.clear();
     m_timers_lookup.clear();
--- qt5test/QTimerInterface/QTimerInterface.h
+++ qt5test/QTimerInterface/QTimerInterface.h
@@ -30,19 +30,19 @@ class QTimerInterface: public QObject, public TimerInterface
                 sc_eventid m_event;
                 sc_boolean m_periodic;
 
-                bool operator==(const CallbackInfo& rhs) const
-                {
-                    return m_state_machine == rhs.m_state_machine
-                            && m_event == rhs.m_event;
-                }
                 bool operator<(const CallbackInfo& rhs) const
                 {
-                    return m_event < rhs.m_event
-                            && m_state_machine < rhs.m_state_machine;
+                    return (m_event < rhs.m_event) ||
+                           (m_event == rhs.m_event && m_state_machine < rhs.m_state_machine);
+                }
+                bool operator==(const CallbackInfo& rhs) const
+                {
+                    return (m_event == rhs.m_event)
+                            && (m_state_machine == rhs.m_state_machine);
                 }
         };
-        typedef QMap<int, CallbackInfo> callback_map_type;
-        typedef QMap<CallbackInfo, int> callback_reverse_map_type;
+        typedef std::map<int, CallbackInfo> callback_map_type;
+        typedef std::map<CallbackInfo, int> callback_reverse_map_type;
         callback_map_type m_timers;
         callback_reverse_map_type m_timers_lookup;
 
